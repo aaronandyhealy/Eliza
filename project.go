311d0dd53998524964c0557ab477f0577f52402e
AARON HEALY - Student <G00333148@gmit.ie>
2017-11-20 18:06:53 +0000

    20th

--- project.go
+++ project.go
@@ -20,19 +20,35 @@ func elizaResponse(input string )string {
 		return "\n Ireland will 1-0. Shane Duffy will score a header"
 	}
 
+
+	if matched, _ := regexp.MatchString(`(?i).*\b[fF]ootball\b.*`, input); matched{
+		responses := []string {
+		"Do you often think of this ?",
+		"Does thinking of (i) bring anything else to mind ?",
+		"What else do you recollect ?",
+		"Why do you remember (i) just now ?",
+		"What in the present situation reminds you of (i) ?",
+		"What is the connection between me and (i) ?",
+		"What else does (i) remind you of ?",
+	}
+
+		randIndex := rand.Intn(len(responses))
+		return responses[randIndex]
+	}
+
 	responses := []string {
-		"\nI’m not sure what you’re trying to say. Could you explain it to me?",
-		"\nHow does that make you feel?",
-		"\nWhy do you say that?",
-		"\nAre you sure",
+		"I'm not sure I understand you fully.",
+		"Please go on.",
+		"What does that suggest to you ?",
+		"Do you feel strongly about discussing such things ?",
+		"That is interesting.  Please continue.",
+		"Tell me more about that.",
+		"Does talking about this bother you ?",
 	}
 
 	randIndex := rand.Intn(len(responses))
 	return responses[randIndex]
 }
-
-
-
 
 func main() {
 	rand.Seed(time.Now().Unix()) 
